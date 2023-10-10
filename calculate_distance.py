dcc4f31d53eb6769ecf72be6335a31017506f4e4
NhiDinh <thaonhi6991@gmail.com>
2023-10-10 16:33:30 +0300

    Delete the old functions

--- calculate_distance.py
+++
@@ -1,6 +0,0 @@
-from geopy.distance import geodesic
-def calculate_distance(current_location, destination):
-    now_location = get_dict_airportcoordinate_by_country(current_location).get(current_location)
-    now_destination = get_dict_airportcoordinate_by_country(destination).get(destination)
-    distance = geodesic(now_location,now_destination).km
-    return distance
